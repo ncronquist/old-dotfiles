1b52ffe3481df16517f9a4b44a2729919c09712f
Nick Cronquist <nick.cronquist@sap.com>
2019-09-19 16:34:44 -0400

    More ubuntu emulator fixes

--- vimrc
+++ vimrc
@@ -313,6 +313,7 @@ vnoremap <leader>gb :Gblame<CR>
 nnoremap <leader>gb :Gblame<CR>
 
 " ==================== vim-go ====================
+let g:go_version_warning = 0
 let g:go_fmt_fail_silently = 0
 let g:go_fmt_command = "goimports"
 let g:go_list_type = "quickfix"
--- zsh/configs/keybindings.zsh
+++ zsh/configs/keybindings.zsh
@@ -16,8 +16,8 @@ bindkey "^N" insert-last-word
 bindkey "^Q" push-line-or-edit
 
 # Bind Up and Down arrow keys to searching through history
-bindkey '^[[A' history-substring-search-up
-bindkey '^[[B' history-substring-search-down
+bindkey "$terminfo[kcuu1]" history-substring-search-up
+bindkey "$terminfo[kcud1]" history-substring-search-down
 
 bindkey "^[[1;5C" forward-word
 bindkey "^[[1;5D" backward-word
